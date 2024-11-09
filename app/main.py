85207e47e2edd617fb2de15f7505fdda9494d6b8
Rilliat <makarovskiyda@gmail.com>
2024-11-09 22:17:19 +0300

    Commit

--- app/main.py
+++ app/main.py
@@ -16,6 +16,21 @@ path_to_json = os.path.join(parent_dir, 'students.json')
 
 app = FastAPI()
 
+strings = {
+    'invalid_token': {
+        'status': 'fail',
+        'result': 'Invalid token',
+    },
+    'home': {
+        'message': 'Hello on home page!',
+    },
+    'result': {
+        'status': 'success',
+        'result': '{}',
+    },
+
+}
+
 
 @app.get("/students")
 def get_all_students():
@@ -23,20 +38,15 @@ def get_all_students():
 
 @app.get("/")
 def home_page():
-    return "Hello on home page!"
+    return strings['home']
 
 @app.get("/get_status")
 def get_status(token: str, service: str):
-    if validate_token(token):
-        result = subprocess.run(["systemctl", "is-active", service if service.endswith('.service') else service+'.service'],
-                                capture_output=True, text=True)
-        result = result.stdout.replace('\n', '')
-        return {
-            "status": "ok",
-            "result": result,
-        }
-    else:
-        return {
-            "status": "fail",
-            "result": "Invalid token"
-        }
+    if not validate_token(token):
+        return strings['invalid_token']
+
+    result = subprocess.run(["systemctl", "is-active", service if service.endswith('.service') else service+'.service'],
+                            capture_output=True, text=True)
+    result = result.stdout.replace('\n', '')
+    return strings['result'].__format__(result)
+
